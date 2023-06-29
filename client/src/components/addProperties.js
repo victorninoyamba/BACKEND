6b4f6e8a5e7fb73cb3094736bb6d3588e2bba656
victorninoyamba <victorninoyamba@gmail.com>
2023-06-29 11:22:17 +0800

    with error

--- client/src/components/addProperties.js
+++ client/src/components/addProperties.js
@@ -88,10 +88,13 @@ export default function AddProperties() {
         console.log(data, "propertyAdded");
         if (data.status == "ok") {
           alert("Property Added!");
+          window.location.reload();
         } else {
           alert("Something went wrong");
         }
       });
+
+      
     } else {
       alert("Please fill in all fields");
     }
@@ -181,23 +184,17 @@ export default function AddProperties() {
             
         </div>
 
-        {/* <div className="pt-4 pb-3 flex items-center justify-center w-full">
-            <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-             <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                <svg aria-hidden="true" className="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-                 <p className="mb-1 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                 <p className="text-xs text-gray-500 dark:text-gray-400">You can upload up to 5 images.</p>
-             </div>
-             <input id="dropzone-file" type="file" className="hidden" />
-             </label>
-        </div>  */}
+          {/* <div className="pt-4 pb-3 flex items-center justify-center w-full">
+              <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                  <svg aria-hidden="true" className="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+                  <p className="mb-1 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                  <p className="text-xs text-gray-500 dark:text-gray-400">You can upload up to 5 images.</p>
+              </div>
+              <input id="dropzone-file" type="file" className="hidden" />
+              </label>
+          </div>  */}
 
-        <div className="flex items-start mb-6">
-        <div className="flex items-center h-5">
-        <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required/>
-        </div>
-        <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">Terms and Conditions</a>.</label>
-    </div>
     <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Upload</button>
 </form>
 </div>
